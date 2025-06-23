50a50506cfd825fe529c753b6f6e0629155489b9
rajzite <159891962+rajzite@users.noreply.github.com>
2025-06-23 11:09:58 +0530

    Create ArraySum.java

---
+++ ArraySum.java
@@ -0,0 +1,11 @@
+//  Sum of Array
+class Main {
+    public static void main(String[] args) {
+        int[] arr = {18,19,20,21,22};
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum += arr[i];
+        }
+        System.out.println(sum);
+    }
+}
